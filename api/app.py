a60a665042eb9351aebe5a36074f6418833fb643
rudnam <francisalbarracin2@gmail.com>
2023-06-05 08:49:48 +0800

    Rewrite file loading

--- api/app.py
+++ api/app.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
 # Load the KMeans model from the pickle file
-with open('TEST_kmeans_model.pkl', 'rb') as file:
+file_path = os.path.join(os.getcwd(), 'files', 'TEST_kmeans_model.pkl')
+with open(file_path, 'rb') as file:
     kmeans_model = pickle.load(file)
 
 # Load the KMeans clustering results from the pickle file
-with open('TEST_kmeans_clustering.pkl', 'rb') as file:
+file_path = os.path.join(os.getcwd(), 'files', 'TEST_kmeans_clustering.pkl')
+with open(file_path, 'rb') as file:
     kmeans_clustering = pickle.load(file)
 
 @app.route('/', methods=['GET', 'POST'])
