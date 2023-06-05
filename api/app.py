57515a581f6246e74d52b3fe1a7232a66a793b2d
rudnam <francisalbarracin2@gmail.com>
2023-06-05 09:23:06 +0800

    Test joblib

--- api/app.py
+++ api/app.py
@@ -1,19 +1,24 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
-import os
+import requests
+import joblib
+from io import BytesIO
+
 
 app = Flask(__name__)
 
 # Load the KMeans model from the pickle file
-file_path = os.path.join(os.getcwd(), 'files', 'TEST_kmeans_model.pkl')
-with open(file_path, 'rb') as file:
-    kmeans_model = pickle.load(file)
+url = 'https://raw.githubusercontent.com/rudnam/cs180-project/main/api/files/TEST_kmeans_model.pkl'
+kmeans_model = joblib.load(BytesIO(requests.get(url).content))
+# with open('TEST_kmeans_model.pkl', 'rb') as file:
+#     kmeans_model = pickle.load(file)
 
 # Load the KMeans clustering results from the pickle file
-file_path = os.path.join(os.getcwd(), 'files', 'TEST_kmeans_clustering.pkl')
-with open(file_path, 'rb') as file:
-    kmeans_clustering = pickle.load(file)
+url = 'https://raw.githubusercontent.com/rudnam/cs180-project/main/api/files/TEST_kmeans_clustering.pkl'
+kmeans_clustering = joblib.load(BytesIO(requests.get(url).content))
+# with open('TEST_kmeans_clustering.pkl', 'rb') as file:
+#     kmeans_clustering = pickle.load(file)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
