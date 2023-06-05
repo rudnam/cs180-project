3b35c4524dfe82ff11eeaf28ff8f0b8a5e4b8d97
rudnam <francisalbarracin2@gmail.com>
2023-06-05 08:45:08 +0800

    Rename files

--- api/app.py
+++ api/app.py
@@ -4,23 +4,13 @@ import numpy as np
 
 app = Flask(__name__)
 
-# # Load the KMeans model from the pickle file
-# with open('_TEST_kmeans_model.pkl', 'rb') as file:
-#     kmeans_model = pickle.load(file)
+# Load the KMeans model from the pickle file
+with open('TEST_kmeans_model.pkl', 'rb') as file:
+    kmeans_model = pickle.load(file)
 
-# # Load the KMeans clustering results from the pickle file
-# with open('_TEST_kmeans_clustering.pkl', 'rb') as file:
-#     kmeans_clustering = pickle.load(file)
-
-from sklearn.cluster import KMeans
-
-kmeans_model = KMeans(n_clusters=3)
-
-# Sample data
-X = np.array([[1, 2], [1.5, 1.8], [5, 8], [8, 8], [1, 0.6], [9, 11]])
-
-# Fit the model with the sample data
-kmeans_clustering = kmeans_model.fit_predict(X)
+# Load the KMeans clustering results from the pickle file
+with open('TEST_kmeans_clustering.pkl', 'rb') as file:
+    kmeans_clustering = pickle.load(file)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
