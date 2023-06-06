bd1287e97322b98830835421284e287d92794aaa
rudnam <francisalbarracin2@gmail.com>
2023-06-06 20:41:14 +0800

    Add randomize button

--- app/static/predict.js
+++ app/static/predict.js
@@ -33,3 +33,37 @@ fetch("static/categorical_options.json")
 			}
 		}
 });
+
+// Function to randomize the values of the form inputs
+function randomizeFormValues() {
+	const incomeForm = document.getElementsByClassName('form-grid')[0];
+
+	function getRandomValue(min, max) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+	
+	for (const container of incomeForm.children) {
+		for (const child of container.children) {
+			if (child.classList.contains('slider')) {
+				let slider = child;
+				let sliderDisplay = document.getElementById(slider.id + '-display');
+	
+				slider.value = getRandomValue(slider.min, slider.max);
+				sliderDisplay.value = slider.value;
+			} else if (child.tagName === 'SELECT') {
+				let select = child;
+				select.selectedIndex = Math.floor(Math.random() * select.options.length);
+				while (select.options[select.selectedIndex].disabled === true) {
+					select.selectedIndex = Math.floor(Math.random() * select.options.length);
+				}
+				
+			}
+		}
+	}
+	
+}
+
+var randomizeButton = document.getElementById("randomize-button");
+randomizeButton.addEventListener("click", randomizeFormValues);
