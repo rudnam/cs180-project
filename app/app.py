aee731203e5619456c63c43eda14b35d81e323d9
rudnam <francisalbarracin2@gmail.com>
2023-06-06 18:03:34 +0800

    Add home, about, and contact

--- app/app.py
+++ app/app.py
@@ -5,28 +5,12 @@ import numpy as np
 
 app = Flask(__name__)
 
-# Load the KMeans model from the pickle file
-with open('TEST_kmeans_model.pkl', 'rb') as file:
-    test_kmeans_model = pickle.load(file)
-
-# Load the KMeans clustering results from the pickle file
-with open('TEST_kmeans_clustering.pkl', 'rb') as file:
-    test_kmeans_clustering = pickle.load(file)
-
 transfer_df_income_path = 'transfer_df_income.pkl'
 transfer_data_path = 'transfer_data.pkl'
 cluster_predictor = ClusterPredictor(transfer_data_path, transfer_df_income_path, 7)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
-    if request.method == 'POST':
-        x = float(request.form.get('x'))
-        y = float(request.form.get('y'))
-        point = np.array([[x, y]])
-        cluster_label = test_kmeans_model.predict(point)[0]
-        result = {'x': x, 'y': y, 'cluster_label': cluster_label}
-        return render_template('index.html', result=result)
-
     return render_template('index.html')
 
 @app.route('/predict', methods=['GET', 'POST'])
@@ -38,6 +22,14 @@ def predict():
         return render_template('predict.html', result=result)
 
     return render_template('predict.html')
+
+@app.route('/contact', methods=['GET'])
+def contact():
+    return render_template('contact.html')
+
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
